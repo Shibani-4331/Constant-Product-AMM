179749cf2921cae2ebc04ebb90e14cea0c479556
Shibani Mishra <sibanimishra2007@gmail.com>
2026-06-26 10:25:43 +0530

    scaffold anchor project

---
+++ programs/constant_product_amm/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5PggfhjuVtYnC8KoAKJKchVXDWssRTvLnsHb3WHzGygQ");
+
+#[program]
+pub mod constant_product_amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
